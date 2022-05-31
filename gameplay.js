24ed3551236c766ad47734f583c79e9c6e5397c8
Aname326 <chan.g2@maristcollege.school.nz>
2022-06-01 11:10:09 +1200

    does not work

--- gameplay.js
+++ gameplay.js
@@ -1,2 +1,11 @@
+$('gameplay.html').ready(function() {
+    $('body').on(keydown, function() {
+        var j = e.keyCode || e.which;
 
+        if (j == "A") {
+            ('meanupage').click()
+        }
+            
+    }
+});
 
