7611bb4ccff631b72d336cd6b2e2aa199115ca96
Aname326 <chan.g2@maristcollege.school.nz>
2022-06-21 09:44:27 +1200

    changing file that goes to when press a

--- gameplay.js
+++ gameplay.js
@@ -3,10 +3,10 @@ document.addEventListener('keyup',(event) => {
 
 
     if (menu === "a") {
-        open('menu1.html')
+        open('menu.html')
     } 
     else if (menu === "A") {
-        open('menu1.html')
+        open('menu.html')
     }
 })
 // when press "a" or "A" it opens menu page in a new tab
