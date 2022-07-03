b194fb0dbd780d6bfdd8eac205967d5253b36c67
Aname326 <chan.g2@maristcollege.school.nz>
2022-07-03 20:35:19 +1200

    doesnt work and will give up for now

--- createacc.js
+++ createacc.js
@@ -1,5 +1,10 @@
 var eyeIcon = document.querySelector("#eye")
+var password = document.querySelector("#password")
+var text = document.querySelector("")
+
+alert("Testing");
 
 function switchseen() {
-    
+    console.log(password)
+    password.type = text
 }
