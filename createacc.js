406dfde47a803c0daabbe95321ca635297fd5a68
Aname326 <chan.g2@maristcollege.school.nz>
2022-07-06 08:31:43 +1200

    removing eye icon

--- createacc.js
+++ createacc.js
@@ -1,9 +1,1 @@
-alert("Testing");
 
-var eyeUnsee = document.querySelector("#eye");
-var password = document.querySelector("#password");
-
-const eyeSee = document.createElement('p');
-eyeSee.innerHTML = "hi";
-
-// not working
