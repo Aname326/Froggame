aff3f2add5aa48ac234a2cd89e47183a3a9e551f
Aname326 <chan.g2@maristcollege.school.nz>
2022-07-06 18:02:42 +1200

    copy and paste return button into chapter 1

---
+++ chapters.js
@@ -0,0 +1,12 @@
+document.addEventListener('keyup',(event) => {
+    var menu = event.key;
+
+
+    if (menu === "a") {
+        open('menu.html')
+    } 
+    else if (menu === "A") {
+        open('menu.html')
+    }
+})
+// when press "a" or "A" it opens menu page in a new tab
