19eda22150e01cf421b87f321e9ce98cd425b792
Aname326 <chan.g2@maristcollege.school.nz>
2022-08-20 12:03:28 +1200

    moved js to js script

--- database.js
+++ database.js
@@ -1,28 +1,23 @@
-type="module"
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-analytics.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAc1ixmC0rgp2NLwWk5h2lfqs66lIY4BjI",
-    authDomain: "frog-game-300fa.firebaseapp.com",
-    projectId: "frog-game-300fa",
-    storageBucket: "frog-game-300fa.appspot.com",
-    messagingSenderId: "311969586464",
-    appId: "1:311969586464:web:d86a712ca4750629384b45",
-    measurementId: "G-417THSVKCG"
+  apiKey: "AIzaSyCIj3YwpMFTfTSguajAPuKPHt_vPHCI81A",
+  authDomain: "frog-game-database.firebaseapp.com",
+  databaseURL: "https://frog-game-database-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "frog-game-database",
+  storageBucket: "frog-game-database.appspot.com",
+  messagingSenderId: "768338091230",
+  appId: "1:768338091230:web:8f7401fe13ca7fe203764f"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 import {getDatabase, set, get, update, remove, ref, child}
-from "https://www.gstatic.com/firebasejs/9.9.0/firebase-database.js"
+from "https://www.gstatic.com/firebasejs/9.9.3/firebase-database.js"
 
 const db = getDatabase();
 
