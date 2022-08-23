1129de65b11650c0c98387610173459c0871e1c3
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2022-08-23 12:09:19 +1200

    all the testing files that was not saved

---
+++ testing.js
@@ -0,0 +1,18 @@
+//function alarm() {
+//    alert ("times up")
+//}
+
+//const minute =  3000
+//in milliseconds so 1000 = 1s
+//let time = 1 * minute
+
+//setTimeout(alarm, time)
+//alert will show in 3 secs sfter reloading the page
+
+
+var text = document.querySelector("#text")
+var image = document.querySelector("#image")
+
+function clickGone() {
+    image.style.display = "none"
+}
