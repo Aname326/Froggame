1f4619c38955909a46d78883aefd321281f8f6ba
Aname326 <chan.g2@maristcollege.school.nz>
2022-10-20 19:08:32 +1300

    adding button for bgm & deleting not used files

--- chapters.js
+++ chapters.js
@@ -12,3 +12,11 @@ document.addEventListener('keyup',(event) => {
 // when press "a" or "A" it opens menu page in a new tab
 
 
+function bgm() {
+    let BGM = new sound ("the-cursed-8.mp3")
+    BGM.play()
+}
+
+var musicBtn = document.getElementById("musicBtn")
+
+document.addEventListener('click', bgm)
